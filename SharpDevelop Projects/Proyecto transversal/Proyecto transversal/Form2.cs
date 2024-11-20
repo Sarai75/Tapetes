ebdbb59cccde107711edd29dd1510acc6aea6272
Sarai75 <moaj080602mmcrlsa3@cbtis217.edu.mx>
2024-11-20 08:22:40 -0600

    Codigo creado para pasar al otro form

---
+++ SharpDevelop Projects/Proyecto transversal/Proyecto transversal/Form2.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC30
+ * Date: 20/11/2024
+ * Time: 08:21 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Proyecto_transversal
+{
+	/// <summary>
+	/// Description of Form2.
+	/// </summary>
+	public partial class Form2 : Form
+	{
+		public Form2()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
