d24af9552dd33afbd4e70a40b1e2dc97f767b4df
Sarai75 <moaj080602mmcrlsa3@cbtis217.edu.mx>
2024-11-21 22:23:58 -0600

    DETALLES 2

--- SharpDevelop Projects/Proyecto transversal/Proyecto transversal/Form2.Designer.cs
+++ SharpDevelop Projects/Proyecto transversal/Proyecto transversal/Form2.Designer.cs
@@ -36,54 +36,45 @@ namespace Proyecto_transversal
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
+			this.lblpintura = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
+			this.lblfrutos = new System.Windows.Forms.Label();
 			this.label6 = new System.Windows.Forms.Label();
-			this.label7 = new System.Windows.Forms.Label();
+			this.lblflores = new System.Windows.Forms.Label();
 			this.label8 = new System.Windows.Forms.Label();
 			this.label9 = new System.Windows.Forms.Label();
 			this.label10 = new System.Windows.Forms.Label();
 			this.label11 = new System.Windows.Forms.Label();
 			this.label12 = new System.Windows.Forms.Label();
-			this.label13 = new System.Windows.Forms.Label();
+			this.lbltrabajadores = new System.Windows.Forms.Label();
 			this.label14 = new System.Windows.Forms.Label();
-			this.label15 = new System.Windows.Forms.Label();
+			this.lblagua = new System.Windows.Forms.Label();
 			this.label16 = new System.Windows.Forms.Label();
 			this.label17 = new System.Windows.Forms.Label();
 			this.label18 = new System.Windows.Forms.Label();
 			this.btncalcular = new System.Windows.Forms.Button();
 			this.btnvolver = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.txtlargo = new System.Windows.Forms.TextBox();
+			this.txtancho = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.lbltorta = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnlimpiar = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
-			// label1
+			// lblpintura
 			// 
-			this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(12, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(163, 23);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Bulto de aserrin";
-			// 
-			// label2
-			// 
-			this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(113, 32);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(62, 23);
-			this.label2.TabIndex = 1;
-			this.label2.Text = "- - - -";
-			// 
-			// label3
-			// 
-			this.label3.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(113, 114);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(62, 23);
-			this.label3.TabIndex = 3;
-			this.label3.Text = "- - - -";
+			this.lblpintura.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblpintura.Location = new System.Drawing.Point(83, 123);
+			this.lblpintura.Name = "lblpintura";
+			this.lblpintura.Size = new System.Drawing.Size(62, 23);
+			this.lblpintura.TabIndex = 3;
+			this.lblpintura.Text = "0";
 			// 
 			// label4
 			// 
@@ -94,14 +85,14 @@ namespace Proyecto_transversal
 			this.label4.TabIndex = 2;
 			this.label4.Text = "litros de pintura";
 			// 
-			// label5
+			// lblfrutos
 			// 
-			this.label5.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label5.Location = new System.Drawing.Point(113, 183);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(62, 23);
-			this.label5.TabIndex = 5;
-			this.label5.Text = "- - - -";
+			this.lblfrutos.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblfrutos.Location = new System.Drawing.Point(83, 195);
+			this.lblfrutos.Name = "lblfrutos";
+			this.lblfrutos.Size = new System.Drawing.Size(62, 23);
+			this.lblfrutos.TabIndex = 5;
+			this.lblfrutos.Text = "0";
 			// 
 			// label6
 			// 
@@ -112,14 +103,14 @@ namespace Proyecto_transversal
 			this.label6.TabIndex = 4;
 			this.label6.Text = "kg de frutos secos";
 			// 
-			// label7
+			// lblflores
 			// 
-			this.label7.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label7.Location = new System.Drawing.Point(113, 248);
-			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(62, 23);
-			this.label7.TabIndex = 7;
-			this.label7.Text = "- - - -";
+			this.lblflores.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblflores.Location = new System.Drawing.Point(83, 268);
+			this.lblflores.Name = "lblflores";
+			this.lblflores.Size = new System.Drawing.Size(62, 23);
+			this.lblflores.TabIndex = 7;
+			this.lblflores.Text = "0";
 			// 
 			// label8
 			// 
@@ -133,11 +124,11 @@ namespace Proyecto_transversal
 			// label9
 			// 
 			this.label9.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label9.Location = new System.Drawing.Point(476, 207);
+			this.label9.Location = new System.Drawing.Point(478, 225);
 			this.label9.Name = "label9";
 			this.label9.Size = new System.Drawing.Size(62, 23);
 			this.label9.TabIndex = 15;
-			this.label9.Text = "- - - -";
+			this.label9.Text = "0";
 			// 
 			// label10
 			// 
@@ -155,48 +146,48 @@ namespace Proyecto_transversal
 			this.label11.Name = "label11";
 			this.label11.Size = new System.Drawing.Size(62, 23);
 			this.label11.TabIndex = 13;
-			this.label11.Text = "- - - -";
+			this.label11.Text = "0";
 			// 
 			// label12
 			// 
 			this.label12.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label12.Location = new System.Drawing.Point(340, 160);
+			this.label12.Location = new System.Drawing.Point(340, 150);
 			this.label12.Name = "label12";
 			this.label12.Size = new System.Drawing.Size(163, 33);
 			this.label12.TabIndex = 12;
 			this.label12.Text = "ingresa el tamaño en mentros";
 			// 
-			// label13
+			// lbltrabajadores
 			// 
-			this.label13.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label13.Location = new System.Drawing.Point(441, 114);
-			this.label13.Name = "label13";
-			this.label13.Size = new System.Drawing.Size(62, 23);
-			this.label13.TabIndex = 11;
-			this.label13.Text = "- - - -";
+			this.lbltrabajadores.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbltrabajadores.Location = new System.Drawing.Point(408, 41);
+			this.lbltrabajadores.Name = "lbltrabajadores";
+			this.lbltrabajadores.Size = new System.Drawing.Size(62, 23);
+			this.lbltrabajadores.TabIndex = 11;
+			this.lbltrabajadores.Text = "0";
 			// 
 			// label14
 			// 
 			this.label14.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label14.Location = new System.Drawing.Point(340, 91);
+			this.label14.Location = new System.Drawing.Point(340, 9);
 			this.label14.Name = "label14";
 			this.label14.Size = new System.Drawing.Size(163, 23);
 			this.label14.TabIndex = 10;
 			this.label14.Text = "n° trabajadores";
 			// 
-			// label15
+			// lblagua
 			// 
-			this.label15.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label15.Location = new System.Drawing.Point(441, 32);
-			this.label15.Name = "label15";
-			this.label15.Size = new System.Drawing.Size(62, 23);
-			this.label15.TabIndex = 9;
-			this.label15.Text = "- - - -";
+			this.lblagua.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblagua.Location = new System.Drawing.Point(83, 46);
+			this.lblagua.Name = "lblagua";
+			this.lblagua.Size = new System.Drawing.Size(62, 23);
+			this.lblagua.TabIndex = 9;
+			this.lblagua.Text = "0";
 			// 
 			// label16
 			// 
 			this.label16.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label16.Location = new System.Drawing.Point(340, 9);
+			this.label16.Location = new System.Drawing.Point(12, 11);
 			this.label16.Name = "label16";
 			this.label16.Size = new System.Drawing.Size(163, 23);
 			this.label16.TabIndex = 8;
@@ -235,12 +226,117 @@ namespace Proyecto_transversal
 			// 
 			this.btnvolver.BackColor = System.Drawing.Color.Silver;
 			this.btnvolver.Font = new System.Drawing.Font("Perpetua Titling MT", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnvolver.Location = new System.Drawing.Point(131, 313);
+			this.btnvolver.Location = new System.Drawing.Point(24, 313);
 			this.btnvolver.Name = "btnvolver";
 			this.btnvolver.Size = new System.Drawing.Size(188, 28);
 			this.btnvolver.TabIndex = 19;
 			this.btnvolver.Text = "volver a precios";
 			this.btnvolver.UseVisualStyleBackColor = false;
+			this.btnvolver.Click += new System.EventHandler(this.BtnvolverClick);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(24, 114);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(35, 34);
+			this.button2.TabIndex = 21;
+			this.button2.Text = "-";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(24, 186);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(35, 34);
+			this.button3.TabIndex = 22;
+			this.button3.Text = "-";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(24, 259);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(35, 34);
+			this.button4.TabIndex = 23;
+			this.button4.Text = "-";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(350, 30);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(35, 34);
+			this.button5.TabIndex = 24;
+			this.button5.Text = "-";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(350, 102);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(35, 34);
+			this.button6.TabIndex = 25;
+			this.button6.Text = "-";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.Button6Click);
+			// 
+			// txtlargo
+			// 
+			this.txtlargo.Location = new System.Drawing.Point(350, 228);
+			this.txtlargo.Name = "txtlargo";
+			this.txtlargo.Size = new System.Drawing.Size(43, 20);
+			this.txtlargo.TabIndex = 26;
+			// 
+			// txtancho
+			// 
+			this.txtancho.Location = new System.Drawing.Point(415, 228);
+			this.txtancho.Name = "txtancho";
+			this.txtancho.Size = new System.Drawing.Size(43, 20);
+			this.txtancho.TabIndex = 27;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(24, 37);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(35, 34);
+			this.button1.TabIndex = 30;
+			this.button1.Text = "-";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// lbltorta
+			// 
+			this.lbltorta.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbltorta.Location = new System.Drawing.Point(408, 111);
+			this.lbltorta.Name = "lbltorta";
+			this.lbltorta.Size = new System.Drawing.Size(62, 23);
+			this.lbltorta.TabIndex = 29;
+			this.lbltorta.Text = "0";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(340, 79);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(163, 23);
+			this.label2.TabIndex = 28;
+			this.label2.Text = "tortas";
+			// 
+			// btnlimpiar
+			// 
+			this.btnlimpiar.BackColor = System.Drawing.Color.Silver;
+			this.btnlimpiar.Font = new System.Drawing.Font("Perpetua Titling MT", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnlimpiar.Location = new System.Drawing.Point(259, 313);
+			this.btnlimpiar.Name = "btnlimpiar";
+			this.btnlimpiar.Size = new System.Drawing.Size(112, 28);
+			this.btnlimpiar.TabIndex = 31;
+			this.btnlimpiar.Text = "limpiar";
+			this.btnlimpiar.UseVisualStyleBackColor = false;
+			this.btnlimpiar.Click += new System.EventHandler(this.BtnlimpiarClick);
 			// 
 			// Form2
 			// 
@@ -248,6 +344,17 @@ namespace Proyecto_transversal
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
 			this.ClientSize = new System.Drawing.Size(552, 353);
+			this.Controls.Add(this.btnlimpiar);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.lbltorta);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtancho);
+			this.Controls.Add(this.txtlargo);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
 			this.Controls.Add(this.btnvolver);
 			this.Controls.Add(this.btncalcular);
 			this.Controls.Add(this.label18);
@@ -256,41 +363,49 @@ namespace Proyecto_transversal
 			this.Controls.Add(this.label10);
 			this.Controls.Add(this.label11);
 			this.Controls.Add(this.label12);
-			this.Controls.Add(this.label13);
+			this.Controls.Add(this.lbltrabajadores);
 			this.Controls.Add(this.label14);
-			this.Controls.Add(this.label15);
+			this.Controls.Add(this.lblagua);
 			this.Controls.Add(this.label16);
-			this.Controls.Add(this.label7);
+			this.Controls.Add(this.lblflores);
 			this.Controls.Add(this.label8);
-			this.Controls.Add(this.label5);
+			this.Controls.Add(this.lblfrutos);
 			this.Controls.Add(this.label6);
-			this.Controls.Add(this.label3);
+			this.Controls.Add(this.lblpintura);
 			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
 			this.Name = "Form2";
 			this.Text = "Form2";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.Button btnlimpiar;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox txtancho;
+		private System.Windows.Forms.TextBox txtlargo;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Button btnvolver;
 		private System.Windows.Forms.Button btncalcular;
 		private System.Windows.Forms.Label label18;
 		private System.Windows.Forms.Label label17;
 		private System.Windows.Forms.Label label16;
-		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.Label lblagua;
 		private System.Windows.Forms.Label label14;
-		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label lbltrabajadores;
 		private System.Windows.Forms.Label label12;
 		private System.Windows.Forms.Label label11;
 		private System.Windows.Forms.Label label10;
 		private System.Windows.Forms.Label label9;
 		private System.Windows.Forms.Label label8;
-		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label lblflores;
 		private System.Windows.Forms.Label label6;
-		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label lblfrutos;
 		private System.Windows.Forms.Label label4;
-		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label lblpintura;
+		private System.Windows.Forms.Label lbltorta;
 	}
 }
--- SharpDevelop Projects/Proyecto transversal/Proyecto transversal/Form2.cs
+++ SharpDevelop Projects/Proyecto transversal/Proyecto transversal/Form2.cs
@@ -28,5 +28,61 @@ namespace Proyecto_transversal
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
 		}
+		
+		void BtnvolverClick(object sender, EventArgs e)
+		{
+			this.Hide();
+			Form1 form1 = new Form1();
+			form1.Show();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			int agua = int.Parse(lblagua.Text);
+            agua++;
+            lblagua.Text = agua.ToString();
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			int pintura = int.Parse(lblpintura.Text);
+            pintura++;
+            lblpintura.Text = pintura.ToString();
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			int frutossecos = int.Parse(lblfrutos.Text);
+            frutossecos++;
+            lblfrutos.Text = frutossecos.ToString();
+		}
+		
+		void Button4Click(object sender, EventArgs e)
+		{
+			int flores = int.Parse(lblflores.Text);
+			flores++;
+            lblflores.Text = flores.ToString();
+		}
+		
+		void Button5Click(object sender, EventArgs e)
+		{
+			int trabajadores = int.Parse(lbltrabajadores.Text);
+            trabajadores++;
+            lbltrabajadores.Text = trabajadores.ToString();
+		}
+		
+		void Button6Click(object sender, EventArgs e)
+		{
+			int torta = int.Parse(lbltorta.Text);
+			torta++;
+            lbltorta.Text = torta.ToString();
+		}
+		
+		
+		void BtnlimpiarClick(object sender, EventArgs e)
+		{
+			txtancho.Clear();
+			txtlargo.Clear();
+		}
 	}
 }
